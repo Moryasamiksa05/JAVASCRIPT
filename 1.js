26a77379798ba023d0fa993908b408bb41936216
Moryasamiksha05 <mauryasamiksha2018@gmail.com>
2023-12-04 13:35:52 +0530

    first commit

---
+++ 1.js
@@ -0,0 +1,21 @@
+//console.log("samiksha");
+
+ let samiksha="20";
+console.log("samiksha");
+const age="22";//cant be redeclared 
+console.log("age");
+//const,let ,var are used before the variables let const are mostly used
+
+
+
+
+const student={
+    fullName:"samiksha maurya",
+    age:20,
+    cgpa:4.6,
+    isPass:true,
+
+};
+
+ student["age"]=student["age"] +1;
+console.log(student["age"]);
